22dc5611ba99db5ba6d370439aee91e1d28d7b6e
Artyom Mikayelyan <artyom.mikayelyan@padcllc.com>
2022-11-23 19:07:33 +0400

    added life location detecting

---
+++ src/app/app.component.ts
@@ -0,0 +1,291 @@
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs';
+import { io } from "socket.io-client";
+import * as $ from "jquery";
+
+declare var ymaps: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Yandex';
+
+  public myPlacemark: any;
+  public currentPlaceLocation: any;
+  public searchData: any;
+  public coordinates: any;
+  public myMap: any;
+  public mySearchControl: any;
+  public currentPlace: any
+  public detectMyCurrentPlaceLocaion: [] = []
+  public from: any
+  public from_location: {} = {}
+  public to: any;
+  public to_location: {} = {};
+  public control: any;
+  public getDistanceTime: any;
+  public multiRoutePromise: any;
+  public socket: any;
+  public socketObj = {}
+  public gettrip: any = []
+  public currentlocation: any;
+  public address: any
+  public mobilelog: any = {}
+
+  constructor() {
+    this._inItYandexMap = this._inItYandexMap.bind(this);
+
+  }
+
+  public ngOnInit(): void {
+
+    this.lifeGeoLocation();
+    this.watchPosition();
+    this.socketConnectionOnMessage();
+    ymaps.ready(this._inItYandexMap)
+
+  }
+
+  public socketConnectionOnMessage(): void {
+    this.socket = io("ws://localhost:80");
+    this.socket.on("onMessage", (arg: any) => {
+      console.log(arg)
+    })
+  }
+
+  public lifeGeoLocation(): void {
+    if (!navigator.geolocation) {
+      alert("location is not supported")
+    }
+    navigator.geolocation.getCurrentPosition((position) => { })
+  }
+
+  public watchPosition(): void {
+    let desLat = 0;
+    let desLot = 0;
+    let id = navigator.geolocation.watchPosition((position) => {
+      let latitude = position.coords.latitude
+      let longitude = position.coords.longitude
+      this.mobilelog = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+      }
+      let y = [latitude, longitude]
+      if (this.myPlacemark) {
+        this.myPlacemark.geometry.setCoordinates(y);
+        ymaps.Placemark(y)
+      } else {
+        this.myPlacemark = this._createPlacemark(y);
+        this.myMap.setCenter(y);
+        this.myMap.geoObjects.add(this.myPlacemark);
+        this.myPlacemark.events.add('dragend', () => {
+          this._getAddress(this.myPlacemark.geometry.getCoordinates());
+        });
+      }
+      this._getAddress(y);
+      if (position.coords.latitude == desLat) {
+        navigator.geolocation.clearWatch(id);
+      }
+    }, (error) => {
+      console.log(error)
+    }, {
+      enableHighAccuracy: true,
+      timeout: 100,
+      maximumAge: 0
+    })
+  }
+
+  private _inItYandexMap(): void {
+    let latitudes = this.mobilelog.latitude;
+    let longitudes = this.mobilelog.longitude;
+
+    // CONNECTION TO MAP
+    var geolocation = ymaps.geolocation;
+    this.myMap = new ymaps.Map('information_map', {
+      center: [latitudes, longitudes],
+      zoom: 9,
+      controls: ["routePanelControl"]
+    }, {
+      searchControlProvider: 'yandex#search'
+    })
+    this.control = this.myMap.controls.get('routePanelControl');
+    this.multiRoutePromise = this.control.routePanel.getRouteAsync();
+
+    //Get My current Location BUTTON
+    var geolocationControl = new ymaps.control.GeolocationControl({
+      data: { content: 'Определить местоположение', image: 'none' },
+      options: { noPlacemark: true, maxWidth: [30, 100, 250], position: { left: 150, top: 650 } }
+    });
+    this.myMap.controls.add(geolocationControl);
+    geolocationControl.events.add('locationchange', (e: any) => {
+      let latitude = this.mobilelog.latitude;
+      let longitude = this.mobilelog.longitude;
+      this._addMarker({ latitude, longitude });
+      this.currentlocation = { latitude, longitude }
+      this.myMap.panTo([latitude, longitude]);
+      this.detectMyCurrentPlaceLocaion = e.get('position')
+      ymaps.geocode(this.detectMyCurrentPlaceLocaion).then((res: any) => {
+        this.from = res.geoObjects.get(0).properties._data.text;
+        this.from_location = {
+          form: this.from,
+          latitude,
+          longitude
+        }
+        this.gettrip = [...this.gettrip, this.from_location]
+        // this.socket.emit("location", {
+        //   from: {
+        //     form: this.from,
+        //     latitude,
+        //     longitude
+        //   }
+        // })
+        this.control.routePanel.state.set({
+          from: this.from,
+        })
+      })
+    });
+
+    // Get GeoCoordinates where clicked
+
+    this.myMap.events.add('click', (e: any) => {
+      const [latitude, longitude] = e.get('coords');
+      this._placemark(e.get('coords'))
+      ymaps.geocode(e.get('coords')).then((res: any) => {
+        this.to = res.geoObjects.get(0).properties._data.text
+        this.control.routePanel.state.set({
+          to: this.to
+        })
+        this.to_location = {
+          to: this.to,
+          latitude,
+          longitude
+        }
+        this.gettrip = [...this.gettrip, this.to_location]
+
+        // this.socket.emit("location", {
+        //   to: {
+        //     to: this.to,
+        //     latitude,
+        //     longitude
+        //   }
+        // })
+      })
+    })
+    if (!this.myPlacemark) {
+    }
+    else {
+      this.myMap.geoObjects.add(this.myPlacemark)
+    }
+
+    let self = this;
+    this.multiRoutePromise.then(function (multiRoute: any) {
+      let distance: any;
+      let duration: any;
+
+      multiRoute.model.events.add('requestsuccess', function () {
+        let activeRoute = multiRoute.getActiveRoute();
+
+        if (activeRoute) {
+          let road: {}
+          distance = activeRoute.properties.get("distance").text;
+          duration = activeRoute.properties.get("duration").text
+          road = {
+            distance,
+            duration
+          }
+          self.gettrip = [...self.gettrip, road]
+          let tripData = self.gettrip
+          // self.socket.emit("location", {
+          // tripData
+          // })
+
+        }
+      });
+    }, (err: any) => {
+      console.log(err);
+    })
+
+    //Get geoLocation via Browser
+    geolocation.get({
+      provider: 'browser',
+      mapStateAutoApply: true
+    }).then((result: any) => {
+      var userAddress = result.geoObjects.get(0).properties.get('text');
+      var userCoodinates = result.geoObjects.get(0).geometry.getCoordinates();
+      console.log(result);
+      console.log(userAddress);
+      console.log(userCoodinates);
+
+      result.geoObjects.options.set('preset', 'islands#blueCircleIcon');
+      console.log(result.geoObjects)
+      this.myMap.geoObjects.add(result.geoObjects)
+    })
+    self.socket.emit("location", {
+
+    })
+  }
+
+  private _placemark(coords: any) {
+    this.myPlacemark = new ymaps.Placemark(coords), {
+    }
+    console.log(this.myPlacemark);
+
+    this.myMap.geoObjects.add(this.myPlacemark)
+    this.currentPlace = coords
+    var placemark = new ymaps.Placemark(this.currentPlace, {
+      iconCaption: 'searching...'
+    }, {
+      draggable: true,
+      iconLayout: 'default#imageWithContent',
+      iconImageHref: './assets/img/car.png',
+      iconImageSize: [42, 54],
+      iconImageOffset: [-20, -50,],
+      iconContentOffset: [15, 15],
+    })
+    console.log(placemark);
+
+    return placemark;
+  }
+
+  private _addMarker(coordinates: { latitude: number, longitude: number }): void {
+    const { latitude, longitude } = coordinates;
+    this.coordinates = [latitude, longitude]
+    const coords = [latitude, longitude];
+
+    if (this.myPlacemark) {
+      this.myPlacemark.geometry.setCoordinates(coords);
+    } else {
+      this.myPlacemark = this._createPlacemark(coords);
+      this.myMap.setCenter(coords);
+      this.myMap.geoObjects.add(this.myPlacemark);
+      // this.myPlacemark.events.add('dragend', () => {
+      //   this._getAddress(this.myPlacemark.geometry.getCoordinates());
+      // });
+    }
+    // this._getAddress(coords);
+  }
+
+  private _createPlacemark(coords: any) {
+    return this._placemark(coords)
+  }
+
+  private _getAddress(coords: any) {
+    this.myPlacemark.properties.set('iconCaption', 'searching...');
+    ymaps.geocode(coords).then((res: any) => {
+      var firstGeoObject = res.geoObjects.get(0);
+      this.myPlacemark.properties
+        .set({
+          iconCaption: [
+            firstGeoObject.getLocalities().length ? firstGeoObject.getLocalities() : firstGeoObject.getAdministrativeAreas(),
+            firstGeoObject.getThoroughfare() || firstGeoObject.getPremise()
+          ].filter(Boolean).join(', '),
+          balloonContent: firstGeoObject.getAddressLine()
+        });
+    });
+  }
+
+
+}
